276f2fece7f3c7a61ae1414ad567de149233de51
Takeshi Miura <ccmiura120@gmail.com>
2021-10-14 15:00:00 +0900

    メールのメッセージ修正

--- amplify/backend/function/sk2sec91b34d93CustomMessage/src/custom.js
+++ amplify/backend/function/sk2sec91b34d93CustomMessage/src/custom.js
@@ -6,7 +6,7 @@ exports.handler = (event, context, callback) => {
     event.response.emailSubject = "セキュリティ講習サイト 招待メール";
     event.response.emailMessage = `ようこそ！ ${event.request.userAttributes.name} さん<br>セキュリティ講習サイトのURLは以下となります。<br>
 url: https://dev.dyj9a9u2n3e2d.amplifyapp.com<br>ユーザー名: ${event.request.usernameParameter}<br>
-メールアドレス: ${event.request.userAttributes["cognito:email_alias"]}<br>
+メールアドレス: ${event.request.userAttributes.email}<br>
 初期パスワード: ${event.request.codeParameter}<br>
 ユーザー名またはメールアドレスをユーザー名として利用できます。<br>
 初回ログイン時にパスワード変更画面に遷移するので、パスワードの変更をお願いします。<br>`;
