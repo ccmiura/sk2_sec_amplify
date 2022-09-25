a2a2b48190db6c8799ec7c0709e65c666d8ab3fe
Takeshi Miura <ccmiura120@gmail.com>
2022-09-25 10:44:58 +0900

    uiのバージョンアップと非推奨修正

--- plugins/amplify.js
+++ plugins/amplify.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
-import Amplify,{ I18n } from 'aws-amplify'
+import { I18n } from 'aws-amplify'
+import {Amplify} from '@aws-amplify/core'
 import '@aws-amplify/ui-vue'
 import awsExports from '@/src/aws-exports.js'
 import { vocabularies } from '@/assets/i18n/amplify/vocabularies';
